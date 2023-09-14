ca5ea380a921aa5157513e1bc6f5c4f54845d842
VincenzoDePetris <vince02@live.it>
2023-09-14 17:06:01 +0200

    add: milestone 0

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from "axios";
+import { store } from "./data/store";
+
+import appHeader from "./components/appHeader.vue";
+import appMain from "./components/appMain.vue";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  components: { appHeader, appMain },
+
+  methods: {
+    fetchFilms(endpoint) {
+      axios.get(endpoint).then((response) => {});
+    },
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <appHeader />
+  <appMain />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped lang="scss"></style>
